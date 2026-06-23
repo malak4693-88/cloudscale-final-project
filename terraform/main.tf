25f61f973f7f9e63afe9bf85b1c335b452daaf88
Malak Abdalrahim <malek_4693@limu.edu.ly>
2026-06-23 19:43:07 +0200

    change vm size

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_B2s"
+    vm_size    = "Standard_B2s_v2"
   }
 
   identity {
