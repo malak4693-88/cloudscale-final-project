d8cb150550ad0fd9d4c6249c26dde4d72b7f6b10
Malak Abdalrahim <malek_4693@limu.edu.ly>
2026-06-23 18:43:27 +0200

    Add Terraform ACR and AKS infrastructure

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "resource_group_name" {
+  description = "Name of the Resource Group"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "acr_name" {
+  description = "Name of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.name
+}
+
+output "acr_login_server" {
+  description = "ACR login server"
+  value       = azurerm_container_registry.acr.login_server
+}
+
+output "aks_cluster_name" {
+  description = "Name of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.name
+}
