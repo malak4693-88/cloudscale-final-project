c8103df02921a8d9e5bb38a1144f0f2820d5b73c
Malak Abdalrahim <malek_4693@limu.edu.ly>
2026-06-27 15:55:00 +0200

    Add Terraform remote backend and align AKS settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,10 +27,16 @@ resource "azurerm_kubernetes_cluster" "aks" {
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = var.aks_cluster_name
 
+  oidc_issuer_enabled = true
+
   default_node_pool {
     name       = "default"
     node_count = 2
     vm_size    = "Standard_B2s_v2"
+
+    upgrade_settings {
+      max_surge = "10%"
+    }
   }
 
   identity {
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "azurerm" {
+    resource_group_name  = "malak-mayar-saja-final-tfstate-rg"
+    storage_account_name = "malakmayarsajatfstate26"
+    container_name       = "tfstate"
+    key                  = "cloudscale-final-project.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
